689f2eeab74c862c06c50a6bfc34b55e27fd1805
thaesuphooko <thaesuphooko@gmail.com>
2026-05-29 12:37:50 +0630

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>ကျွန်တော့်ဆိုင်လေးသို့ ကြိုဆိုပါတယ်</h1>
+      <h1>ကျွန်တော့်ဆိုင်လေးမှ ကြိုဆိုပါတယ်</h1>
       <p>ပစ္စည်းတွေ မကြာခင် ပေါ်လာတော့မှာပါ</p>
     </div>
   );
