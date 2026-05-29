ebd4be362cebe530f15a147c13e8136054187ea4
thaesuphooko <thaesuphooko@gmail.com>
2026-05-29 12:48:07 +0630

    Update route.js

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const products = [{ id: 1, title: 'ပစ္စည်း နမူနာ' }];
+  const products = [
+    { id: 1, title: 'ပစ္စည်း နမူနာ ၁', price: 1000 },
+    { id: 2, title: 'ပစ္စည်း နမူနာ ၂', price: 2000 }
+  ];
   return NextResponse.json(products);
 }
