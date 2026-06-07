e5780d7e51b074d33a967a760660afe802fb2c2e
thaesuphooko <288798044+thaesuphooko@users.noreply.github.com>
2026-06-07 15:52:23 +0630

    Delete route.js

--- src/app/api/products/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const products = [
-    { id: 1, title: 'ပစ္စည်း နမူနာ ၁', price: 1000 },
-    { id: 2, title: 'ပစ္စည်း နမူနာ ၂', price: 2000 }
-  ];
-  return NextResponse.json(products);
-}
